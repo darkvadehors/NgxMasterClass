209dc30ef7d2574b0a7eb4d9d118bdf33b8de79e
FazioNico <contact@nicolasfazio.ch>
2021-06-07 20:49:10 +0200

    upd: add actions ci/cd

---
+++ deploy.js
@@ -0,0 +1,42 @@
+var FtpDeploy = require("ftp-deploy");
+var ftpDeploy = new FtpDeploy();
+
+// config extracting
+const {env: {
+  host = null,
+  user = null,
+  password = null,
+  port = 21,
+  localRoot = __dirname + "/dist/NgxMasterclass",
+  remoteRoot = '/',
+  deleteRemote = true,
+  forcePasv = true
+} = {}} = process||{};
+// build config object data
+var config = {
+  user,
+  // Password optional, prompted if none given
+  password,
+  host,
+  port,
+  localRoot,
+  remoteRoot,
+  // include: ["*", "**/*"],      // this would upload everything except dot files
+  include: ["*", "**/*", ".*"],
+  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+  exclude: [
+    "dist/**/*.map",
+    "node_modules/**",
+    "node_modules/**/.*",
+    ".git/**",
+  ],
+  // delete ALL existing files at destination before uploading, if true
+  deleteRemote,
+  forcePasv
+};
+
+// run lib to deploy
+ftpDeploy
+  .deploy(config)
+  .then((res) => console.log("finished:", res))
+  .catch((err) => console.log(err));
